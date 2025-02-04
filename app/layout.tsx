d655c969c0a0ef337ce936f76ffd2d0a934bd58c
DeliKhan <deluckshanmurugamoorthy@gmail.com>
2025-02-03 20:16:25 -0500

    fix: deployment issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body className="flex flex-col min-h-screen">
         <main 
           className="flex-grow">{children}
